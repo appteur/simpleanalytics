d32a9b9478c9454617a5351f3842b2f46c10bbb8
appteur <notmeyet@nym.hush.com>
2017-05-09 22:33:33 -0700

    Fixed stored files from .gitignore.

--- Example/Pods/Target Support Files/SimpleAnalytics/SimpleAnalytics-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SimpleAnalytics : NSObject
-@end
-@implementation PodsDummy_SimpleAnalytics
-@end
--- Example/Pods/Target Support Files/SimpleAnalytics/SimpleAnalytics-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double SimpleAnalyticsVersionNumber;
-FOUNDATION_EXPORT const unsigned char SimpleAnalyticsVersionString[];
-
